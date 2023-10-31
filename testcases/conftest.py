6938c665e1329650e8bc8dadf5a67b245644fb55
xiu.jiang <xiu.jiang@envision-energy.com>
2023-10-31 17:20:52 +0800

    命名有误

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -4,6 +4,6 @@ from en_galileo_sdk.utility.login import login
 import pytest
 
 @pytest.fixture(scope="session",autouse=True)
-def login():
+def get_token():
     token = login(username='Testplatform', password='pqxY89lw*',account_type=1)
     return token
