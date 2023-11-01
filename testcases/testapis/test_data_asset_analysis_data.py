c5559c85bc3882cf0a34432d259a4d8489c228ad
xiu.jiang <xiu.jiang@envision-energy.com>
2023-11-01 15:28:53 +0800

    修改断言

--- testcases/testapis/test_data_asset_analysis_data.py
+++ testcases/testapis/test_data_asset_analysis_data.py
@@ -10,12 +10,17 @@
 import pandas as pd
 from en_galileo_sdk.data_access.center.data_asset_analysis_data import DataAssetAnalysisData
 from en_galileo_sdk.data_access.center.data_asset_connector import DataAssetConnector
+import pytest
+
+@pytest.fixture(scope="module")
+def get_data_asset_connector(get_token):
+    yield DataAssetConnector(get_token)
 
 class TestDataAssetAnalysisData:
-    def setup_class(self,get_token):
-        self.con = DataAssetConnector(get_token)
 
-    def test_get_data_asset_objects(self):
+    def test_get_data_asset_objects(self,get_data_asset_connector):
         '''查询数据资产列表'''
-        data_asset_objects = self.con.get_data_asset_objects()
-        assert 1==1
+        data_asset_objects = get_data_asset_connector.get_data_asset_objects()
+        df = pd.DataFrame(data_asset_objects)
+        assert df.shape[0] == 96
+        assert df.shape[1] == 7
