1faea9ef65cb674218166005fec93c9b5583ce27
xiu.jiang <xiu.jiang@envision-energy.com>
2023-11-02 17:11:15 +0800

    去掉类

--- testcases/testapis/test_data_asset_analysis_data.py
+++ testcases/testapis/test_data_asset_analysis_data.py
@@ -16,20 +16,18 @@ import pytest
 def get_data_asset_connector(get_token):
     yield DataAssetConnector(get_token)
 
-class TestDataAssetAnalysisData():
-
-    def test_get_data_asset_objects(self,get_data_asset_connector):
-        '''查询数据资产列表'''
-        data_asset_objects = get_data_asset_connector.get_data_asset_objects()
-        df = pd.DataFrame(data_asset_objects)
-        assert df.shape[0] == 96
-        assert df.shape[1] == 7
+def test_get_data_asset_objects(self,get_data_asset_connector):
+    '''查询数据资产列表'''
+    data_asset_objects = get_data_asset_connector.get_data_asset_objects()
+    df = pd.DataFrame(data_asset_objects)
+    assert df.shape[0] == 96
+    assert df.shape[1] == 7
     
-    def test_get_tables(self,get_data_asset_connector):
-        '''查询数据资产的数据模型列表,判断表名包含领域+Layer+Application+资产名'''
-        tables = get_data_asset_connector.get_tables(data_asset_code='WindWorkOrder')
-        df = pd.DataFrame(tables)
-        assert df.shape[0] > 0
-        assert df.shape[1] > 0
-        for idx,row in df.iterrows() :
-            assert "windturbineoperationdata_prod_galileo_windworkorder" in row['name']
+def test_get_tables(self,get_data_asset_connector):
+    '''查询数据资产的数据模型列表,判断表名包含领域+Layer+Application+资产名'''
+    tables = get_data_asset_connector.get_tables(data_asset_code='WindWorkOrder')
+    df = pd.DataFrame(tables)
+    assert df.shape[0] > 0
+    assert df.shape[1] > 0
+    for idx,row in df.iterrows() :
+        assert "windturbineoperationdata_prod_galileo_windworkorder" in row['name']
