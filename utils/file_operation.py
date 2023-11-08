20b1b6f3c1d99cf9c291531fe65612326ed82050
xiu.jiang <xiu.jiang@envision-energy.com>
2023-11-08 14:45:53 +0800

    父目录

--- utils/file_operation.py
+++ utils/file_operation.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-data_dir = os.path.join(os.path.dirname(__file__),"testdata")
+data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),"testdata")
 
 def read_csv_as_dataframe(csv_name):
     df_expect = pd.read_csv(os.path.join(data_dir,csv_name))
