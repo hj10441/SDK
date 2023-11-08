e4ba39a44cabb53de2e34af25d3a161ba9ce997c
xiu.jiang <xiu.jiang@envision-energy.com>
2023-11-08 18:00:58 +0800

    密码加密

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,7 +1,8 @@
 from en_galileo_sdk.utility.login import login
 import pytest
+from utils.encrypt import decrypt
 
 @pytest.fixture(scope="session",autouse=True)
 def get_token():
-    token = login(username='Testplatform', password='pqxY89lw*',account_type=1)
+    token = login(username='Testplatform', password=decrypt('gAAAAABlS1vLzAxn8YVhPDOndyXKmId83nfhIqdLAyxsKBGzswIZxE9LzoO-ZdWZoJyauKHNeDjMy0YulbddSq5EBmj1VR84qA==','a9NIqbxL7YkaKqmhqqzz_15KnjDwLeF02gDIaPqnnzg='),account_type=1)
     return token
